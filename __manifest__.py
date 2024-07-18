3046e29f2135aaafd110ac9b8955bd669a60225c
Anton Berh <anton.berg.it@gmail.com>
2024-07-18 16:19:37 +0300

    FIX to install

--- __manifest__.py
+++ __manifest__.py
@@ -10,14 +10,22 @@
         'demo/hr_job_demo.xml',
         'demo/hr_department_demo.xml',
         'demo/hr_employee_demo.xml',
-        'demo/resource_calendar_demo.xml',
+
+        # TODO: BRGM: there is no such file
+    #    'demo/resource_calendar_demo.xml',
+
         'demo/hr_contract_type_demo.xml',
-        'demo/hr_contract_demo.xml',
+
+        # TODO: BRGM: commented due to errors in file
+    #    'demo/hr_contract_demo.xml',
+
         'demo/hr_employee_position_demo.xml',
     ],
     'data': [
         'views/hr_employee_views.xml',
         'views/hr_job_view.xml',
+
+        # TODO: BRGM: lack of acl - need to grant all 4
         'security/ir.model.access.csv'
     ],
     'assets': {
